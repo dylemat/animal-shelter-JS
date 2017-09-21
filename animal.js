9012ba8334d56dfbe90168fe8a1428e48f3ea59b
Your Name <you@example.com>
2017-09-21 21:43:00 +0100

    add object

--- animal.js
+++ animal.js
@@ -1,25 +1,42 @@
-//schronisko dla zwierzat
-//zadanie z ksiazki junior developer java dev matt
+    // //schronisko dla zwierzat
+    // //zadanie z ksiazki junior developer java dev matt
+    //
+    // var schronisko=[];
+    // function pokaz(){
+    //   console.log(schronisko);
+    // }
+    // function add(gadzina){
+    //   schronisko.push(gadzina);
+    //   pokaz();
+    // }
+    // function change(position,nowy){
+    //   schronisko[position]=nowy;
+    //   pokaz();
+    // }
+    // function usun(position){
+    //   schronisko.splice(position,1);
+    //   pokaz();
+    // }
+    // add('cokolwiek')
+    // add('kot')
+    // add('mysz')
+    // add('komar')
+    //
+    // usun(0,1)
+    // //zmiana tablica na obiekt
+    var schroniskoObject=
+        {
+            schronisko:[],
+          pokaz:function(){
+               console.log(this.schronisko);
+           },
+          add:function(gadzina){
+              this.schronisko.push(gadzina);
+              this.pokaz();
+            },
+        }
 
-var schronisko=[];
-function pokaz(){
-  console.log(schronisko);
-}
-function add(gadzina){
-  schronisko.push(gadzina);
-  pokaz();
-}
-function change(position,nowy){
-  schronisko[position]=nowy;
-  pokaz();
-}
-function usun(position){
-  schronisko.splice(position,1);
-  pokaz();
-}
 
-add('kot')
-add('mysz')
-add('komar')
 
-usun(0,1)
+   schroniskoObject.add('sdad');
+   
